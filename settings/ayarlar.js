dd47737ff62a3eba52d131ed2be90e2adfeb863f
Baronmarka <158518665+Baronmarka@users.noreply.github.com>
2024-02-01 23:50:06 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
+    TOKEN: process.env.TOKEN || "MTIwMjcxMTIyMjQyMDc3MDg3Ng.GZKlk6.s3yQW4g1hJuobkihjinauhq2eAEDGTVRxRz78Q",  // bot token gir :)
     PREFIX: process.env.PREFIX || "!", // bot prefix
-    SAHİP: process.env.SAHİP || "SAHİP İD", //sahip id :()
+    SAHİP: process.env.SAHİP || "1154078232539320350", //sahip id :()
 }
